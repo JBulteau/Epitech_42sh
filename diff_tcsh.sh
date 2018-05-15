c664cf1eda884507dde281ff29e94b15ec7af57b
NahisWayard <lilian.arago@outlook.fr>
2018-05-15 22:24:02 +0200

    Use ./diff_tcsh.sh env [comm] to exec with env -i

--- diff_tcsh.sh
+++ diff_tcsh.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+if [ "$1" = "env" ]
+then
+        env="env -i"
+        shift
+else
+        $env = ""
+fi
+
 echo "--------------"
 echo "-----42sh-----"
 echo "--------------"
-echo $1 | ./42sh; echo $?
+echo $1 | $env ./42sh; echo $?
 echo
 echo "--------------"
 echo "-----tcsh-----"
 echo "--------------"
-echo $1 | tcsh; echo $?
+echo $1 | $env tcsh; echo $?
 echo
