fc6b83f98af33f51836dd6c00068ce84d07ccfee
NahisWayard <lilian.arago@outlook.fr>
2018-05-16 07:26:12 +0200

    Added some basic binnaries

--- tests/diff_tcsh.sh
+++ tests/diff_tcsh.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
+make -C ..
+
 if [ "$1" = "env" ]
 then
         env="env -i"
         shift
-else
-        $env = ""
 fi
 
 echo "--------------"
 echo "-----42sh-----"
 echo "--------------"
-echo $1 | $env ./42sh; echo $?
+echo $1 | $env ../42sh; echo $?
 echo
 echo "--------------"
 echo "-----tcsh-----"
