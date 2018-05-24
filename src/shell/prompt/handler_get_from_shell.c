36badc896e4523976f769914c53a26bb021a0136
mike gerard <mike.gerard27@gmail.com>
2018-05-24 12:36:34 +0200

    add nb of jobs for prompt

--- src/shell/prompt/handler_get_from_shell.c
+++ src/shell/prompt/handler_get_from_shell.c
@@ -39,7 +39,6 @@ int ptr_handle_jobs(shell_t *shell, char *input)
 {
         UNUSED(shell);
         UNUSED(input);
-        //for (; pid_job[i] != NULL; i++);
-        printf("Mike je te laisse faire");
+        printf("%d", get_nb_job());
         return (SUCCESS_RETURN);
 }
