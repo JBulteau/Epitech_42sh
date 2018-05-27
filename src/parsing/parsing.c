458621e8af04de91104b94a95db99c32c0d80d35
Jules <jules.bulteau@epitech.eu>
2018-05-27 09:43:23 +0200

    fix crash when nothing after separator

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -8,6 +8,58 @@
 #include "my.h"
 #include "parsing.h"
 
+comm_t *check_redir(comm_t *comm)
+{
+	for (int i = 0; i < 4; i++) {
+		if (comm->red[i] && comm->red[i]->target == NULL) {
+			free_comm(comm);
+			return (NULL);
+		}
+	}
+	return (comm);
+}
+
+comm_t *check_pipe(comm_t *comm)
+{
+	while (comm->pipe[OUT]) {
+		if ((comm->pipe[OUT]->output = \
+		check_comm(comm->pipe[OUT]->output)) == NULL) {
+			free_comm(comm);
+			return (NULL);
+		}
+	}
+	return (comm);
+}
+
+comm_t *check_next(comm_t *comm)
+{
+	while (comm->next) {
+		if ((comm->next = check_comm(comm->next)) == NULL) {
+			free_comm(comm);
+			return (NULL);
+		}
+	}
+	return (comm);
+}
+
+comm_t *check_comm(comm_t *comm)
+{
+	comm = check_redir(comm);
+	if (comm == NULL)
+		return (NULL);
+	comm = check_pipe(comm);
+	if (comm == NULL)
+		return (NULL);
+	comm = check_next(comm);
+	if (comm == NULL)
+		return (NULL);
+	if ((comm && !comm->argv) || (comm && comm->argv && !comm->argv[0])) {
+		free_comm(comm);
+		return (NULL);
+	}
+	return (comm);
+}
+
 comm_t **parsing(shell_t *shell)
 {
 	node_t *node = init_node(shell->input, NONE);
@@ -24,11 +76,11 @@ comm_t **parsing(shell_t *shell)
 		return (NULL);
 	comm = init_comm_array(comm, node);
 	comm = convert_node(comm, node, shell);
-	if ((comm && !comm[0]) || (comm && comm[0] && !comm[0]->argv) \
-	|| (comm && comm[0] && comm[0]->argv && !comm[0]->argv[0])) {
-		free_comms(comm);
-		comm = NULL;
-	}
+	for (int i = 0; comm && comm[i]; i++)
+		if ((comm[i] = check_comm(comm[i])) == NULL) {
+			free_comms(comm);
+			return (NULL);
+		}
 	free_node(node);
 	return (comm);
 }
